878f956661b3773ad9eaefbcc0034b6ece1c0b58
EdPaillard <ed.paillard@gmail.com>
2022-11-28 15:37:40 +0100

    kube back v0.2

--- back_rf/config/dev.exs
+++ back_rf/config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :back_rf, BackRf.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "psqlrfdb",
-  database: "back_rf_dev",
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  hostname: System.get_env("POSTGRES_HOST"),
+  database: System.get_env("POSTGRES_DB"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
---
+++ back_rf/dockerKube.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# if [ -z "$(ls -A ~/.ssh/perso)" ]; then
+#     mv ~/.ssh/id_rsa* ~/.ssh/epitech
+#     mv ~/.ssh/perso/* ~/.ssh/
+#     echo "Conf Perso loaded"
+# fi
+
+docker build -t 162.19.66.30:7000/randomfun-backend:kube-v0.2 .
+
+docker push 162.19.66.30:7000/randomfun-backend:kube-v0.2
